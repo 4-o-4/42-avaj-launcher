0a7b151a568a89db4f29cc7a9948e7cfef63d10c
4-o-4 <yuzhakov.lev@gmail.com>
2022-04-15 19:25:40 +0300

    fix: type case

--- src/avaj/simulator/air/AircraftFactory.java
+++ src/avaj/simulator/air/AircraftFactory.java
@@ -2,10 +2,11 @@ package avaj.simulator.air;
 
 import avaj.simulator.exception.NumberCheckException;
 import avaj.simulator.tower.Flyable;
+import java.util.Locale;
 
 public abstract class AircraftFactory {
     public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
-        return switch (type) {
+        return switch (type.toLowerCase(Locale.ROOT)) {
             case "baloon" -> new Baloon(name, new Coordinates(longitude, latitude, height));
             case "helicopter" -> new Helicopter(name, new Coordinates(longitude, latitude, height));
             case "jetplane" -> new JetPlane(name, new Coordinates(longitude, latitude, height));
